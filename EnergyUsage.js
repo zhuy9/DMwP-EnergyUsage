419f799eb3c1373f41b7181b624fd42309cd450c
Wenze Ma <maw1@rose-hulman.edu>
2021-01-29 14:30:02 +0800

    finished displaying graphs on webpage

--- EnergyUsage.js
+++ EnergyUsage.js
@@ -9,6 +9,45 @@
             });
             $('#description_img').toggle();
         });
+
+        $('#clickViewGraphs').click(function () {
+            var x = $('#x_label').val()
+            var y = $('#y_label').val()
+
+            if (x == '1' && y == '1') {
+                $('#graph_img').attr('src','Figure/peakDemandSumVsGen.png');
+            } else if (x == '1' && y == '2') {
+                $('#graph_img').attr('src','Figure/peakDemandWinVsGen.png');
+            } else if (x == '1' && y == '3') {
+                $('#graph_img').attr('src','Figure/totGenByState.png');
+            } else if (x == '1' && y == '4') {
+                $('#graph_img').attr('src','Figure/totGenByUType.png');
+            } else if (x == '2' && y == '1') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '2' && y == '2') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '2' && y == '3') {
+                $('#graph_img').attr('src','Figure/powerLossAllKinds.png');
+            } else if (x == '2' && y == '4') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '3' && y == '1') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '3' && y == '2') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '3' && y == '3') {
+                $('#graph_img').attr('src','Figure/totRevenueByState.png');
+            } else if (x == '3' && y == '4') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '4' && y == '1') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '4' && y == '2') {
+                $('#graph_img').attr('src','Figure/summerWinterPeak.png');
+            } else if (x == '4' && y == '3') {
+                $('#graph_img').attr('src','Figure/.png');
+            } else if (x == '4' && y == '4') {
+                $('#graph_img').attr('src','Figure/.png');
+            }
+        });
     }
 
     $(window).on('load', setup);
