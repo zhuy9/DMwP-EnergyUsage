04f342ca11a147f7698939becfc854aa4480e57a
lawrenceko <twlawrenceko@yahoo.com>
2021-02-05 19:08:53 +0800

    Added dynamic dropdown menu for XYPlot and PieChart

--- EnergyUsage.js
+++ EnergyUsage.js
@@ -1,6 +1,21 @@
 (function () {
 
     "use strict";
+    
+    var graphMenuSelection = document.getElementById('graph-type');
+    
+//    function graphMenuDropdown () {
+//     	switch(this.graphMenuSelection) {
+//     		case '1':
+//     			console.log('X-Y Plot selected');
+//     			break;
+//     		case '2':
+//     			console.log('Pie Chart selected');
+//     			break;
+//     	}
+//     }
+//     
+//     graphMenuSelection.addEventListener('change', graphMenuDropdown, false);
 
     function setup() {
         $('#clickViewDescription').click(function () {
@@ -10,11 +25,32 @@
             $('#description_img').toggle();
         });
 
+		$('#changeGraphType').click(function (){
+			var graph = $('#graph-type').val();
+			if (graph == '1') {
+				console.log("graph-type1: "+graph)
+				document.getElementById("selection").style.display = '';
+			} else if (graph == '2') {
+				console.log("graph-type2: "+graph)
+				document.getElementById("selection").style.display = 'none';
+			}
+			
+		});
+
         $('#clickViewGraphs').click(function () {
+        	var graph = $('#graph-type').val();
             var x = $('#x_label').val()
             var y = $('#y_label').val()
 
-            if (x == '1' && y == '1') {
+			if (graph == "1") {
+				//XY Plot
+				document.getElementById("donutchart").style.display = 'none';
+				document.getElementById("donutchart").style.width = "0px";
+				document.getElementById("donutchart").style.height = "0px";
+				
+				document.getElementById("graph_img").style.display = '';
+				
+				if (x == '1' && y == '1') {
                 $('#graph_img').attr('src','Figure/peakDemandSumVsGen.png');
             } else if (x == '1' && y == '2') {
                 $('#graph_img').attr('src','Figure/peakDemandWinVsGen.png');
@@ -47,8 +83,26 @@
             } else if (x == '4' && y == '4') {
                 $('#graph_img').attr('src','Figure/.png');
             }
+			} else if (graph == "2") {
+				// Show Pie Chart
+				document.getElementById("graph_img").style.display = 'none';
+				
+				document.getElementById("donutchart").style.display = '';
+				document.getElementById("donutchart").style.width = "700px";
+				document.getElementById("donutchart").style.height = "400px";
+				
+// 				var div = document.createElement("div");
+// 				div.setAttribute("id", "donutchart")
+// 				div.style.width = "900px";
+// 				div.style.height = "520px";
+// 				document.getElementById("displayArea").appendChild(div);
+			}
+            
+		
         });
     }
 
     $(window).on('load', setup);
 })();
+
+
